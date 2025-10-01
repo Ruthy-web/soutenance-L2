91a80e55a719747841e25646573ea2d889bc65bf
Aurel <gessyken@gmail.com>
2025-10-01 12:41:52 +0100

    Auth is ok

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->string('key')->primary();
             $table->mediumText('value');
             $table->integer('expiration');
-            $table->timestumps();
+            $table->timestamps();
                     });
 
         Schema::create('cache_locks', function (Blueprint $table) {
